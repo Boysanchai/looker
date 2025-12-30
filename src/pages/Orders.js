4322beb2259cfdf567edf20b9967cc6b05539829
Boysanchai <58928197+Boysanchai@users.noreply.github.com>
2025-12-30 09:53:44 +0700

    Update Orders.js

--- src/pages/Orders.js
+++ src/pages/Orders.js
@@ -2,7 +2,7 @@ import { Box, Typography, Paper } from "@mui/material";
 
 export default function Orders() {
   return (
-    <Box sx={{ height: "85%" }}>
+    <Box sx={{ height: "95%" }}>
       <Typography variant="h5" fontWeight={600} mb={1}>
         Orders Dashboard
       </Typography>
@@ -11,7 +11,7 @@ export default function Orders() {
         <iframe
           title="Orders Report"
           src="https://lookerstudio.google.com/embed/reporting/3a960143-c7fb-464a-b66c-ffcce7b20777/page/TWTfF"
-          style={{ width: "80%", height: "70%", border: "none" }}
+          style={{ width: "80%", height: "50%", border: "none" }}
           allowFullScreen
         />
       </Paper>
